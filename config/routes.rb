0bcb2ed14c6acf88eab6433ba79152e4b41cbddd
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-11-01 03:09:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
+<<<<<<< HEAD
   namespace :admin do
+=======
+  scope '/admin' do
+>>>>>>> 210e5442670c4e4a96403efaaf274ff1ec4c6262
     resources :stats, only: [:index]
   end
 
